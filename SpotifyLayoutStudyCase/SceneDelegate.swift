da3b0f93b301cc3fb1644040c284c9c0dea42072
Linda Marie <lindamarie.ribeiro@gmail.com>
2026-05-21 22:54:55 -0300

    Ajuste de layout responsivo na tela de menu e configuração da UIScrollView para suporte a iPhone e iPad

--- SpotifyLayoutStudyCase/SceneDelegate.swift
+++ SpotifyLayoutStudyCase/SceneDelegate.swift
@@ -20,6 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.rootViewController = UINavigationController(rootViewController: StartViewController())
         window?.makeKeyAndVisible()
     }
+    
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
